8290e7d946048037d643aecc8a8fd27f6c2e37aa
Atakan Sentürk <atakan.sent@gmail.com>
2023-05-27 19:36:56 +0200

    removed comments and cleaned up requirements

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#import pypub
-
 from selenium import webdriver
 from mdutils import Html
 from mdutils.mdutils import MdUtils
@@ -7,42 +5,6 @@ from mdutils.mdutils import MdUtils
 from selenium.webdriver.common.by import By
 
 class Main:
-    def array_to_html(arr):
-        title = arr[0]
-        text = '\n'.join(arr[1:])
-        a = ""
-        for part in arr[1:]:
-            a += f"<p>{part}</p>"
-        html = f'''
-        <html>
-        <head>
-        <title>{title}</title>
-        <style>
-        body {{
-            background-color: darkgrey;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin: 0;
-            padding: 0;
-        }}
-
-        .container {{
-            text-align: center;
-            background-color: #f1f1f1;
-            padding: 20px;
-            border-radius: 10px;
-        }}
-        </style>
-        </head>
-        <body>
-        <h1>{title}</h1>
-        {a}
-        </body>
-        </html>
-        '''
-
-        return html
 
     url = "https://lightnovelpub.fan/novel/the-beginning-after-the-end-548/chapter-401-30041322"
 
@@ -59,22 +21,11 @@ class Main:
 
     driver.get(url)
 
-
-    #element = driver.find_element(By.ID, "chapter-container")
     chapter = driver.find_element(By.ID, "chapter-container")
     parts = chapter.text.splitlines()
     chapter_title = parts[0]
-    #parts.pop(0)
-    #print(array_to_html(parts))
-    #soup = BeautifulSoup(response.content, "lxml")
 
-    #links = soup.find_all('a')
-
-    #print(soup)
     print(type(parts[0]))
-
-    #with open("C:\\Users\\ataka\\Documents\\fun\\the-beginning\\" + file_name, "w", encoding="utf8") as my_file:
-    #    my_file.write(array_to_html(parts))
 
     mdFile = MdUtils(file_name=chapter_title, title=chapter_title)
 
